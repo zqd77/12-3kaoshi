1956820e6595290d796b1fedab1218a63258cd8d
zq <759545434@qq.com>
2018-12-03 09:10:51 +0800

    完成压缩css和watch监听

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp');
 var scss = require('gulp-sass');
+var mincss = require('gulp-clean-css');
 gulp.task('scss', function() {
     return gulp.src('./src/scss/index.scss')
         .pipe(scss())
+        .pipe(mincss())
         .pipe(gulp.dest('./src/css'))
+})
+gulp.task('watch', function() {
+    return gulp.watch('./src/scss/*.scss', gulp.series('scss'))
 })
