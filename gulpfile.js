ad28cba3bb31b302f196554af964cc624fa74144
zq <759545434@qq.com>
2018-12-03 09:23:46 +0800

    完成服务器搭建

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,30 @@
 var gulp = require('gulp');
 var scss = require('gulp-sass');
 var mincss = require('gulp-clean-css');
+var server = require('gulp-webserver');
 gulp.task('scss', function() {
     return gulp.src('./src/scss/index.scss')
         .pipe(scss())
         .pipe(mincss())
         .pipe(gulp.dest('./src/css'))
+})
+gulp.task('server', function() {
+    return gulp.src('src')
+        .pipe(server({
+            port: 8000,
+            // open: true,
+            middleware: function(req, res, next) {
+                var pathname = require('url').parse(req.url).pathname;
+                if (pathname === '/favicon.ico') {
+                    return res.end();
+                }
+                if (pathname === '/') {
+                    res.end(require('fs').readFileSync(require('path').join(__dirname, 'src', 'index.html')))
+                } else {
+                    res.end(require('fs').readFileSync(require('path').join(__dirname, 'src', pathname)))
+                }
+            }
+        }))
 })
 gulp.task('watch', function() {
     return gulp.watch('./src/scss/*.scss', gulp.series('scss'))
